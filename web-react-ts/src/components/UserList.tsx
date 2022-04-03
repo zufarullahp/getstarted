fc1901358642b8a07da7265c6dd2f893148987ac
zufarullahp <93693521+zufarullahp@users.noreply.github.com>
2022-04-03 23:29:34 +0700

    Initial commit

---
+++ web-react-ts/src/components/UserList.tsx
@@ -0,0 +1,171 @@
+import React from 'react'
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  SortDirection,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Paper,
+  TableSortLabel,
+  TextField,
+} from '@material-ui/core'
+import { useQuery, gql } from '@apollo/client'
+
+import Title from './Title'
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 700,
+      marginTop: theme.spacing(3),
+      overflowX: 'auto',
+      margin: 'auto',
+    },
+    table: {
+      minWidth: 700,
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      minWidth: 300,
+    },
+  })
+
+const GET_USER = gql`
+  query usersPaginateQuery(
+    $first: Int
+    $offset: Int
+    $orderBy: [UserSort]
+    $filter: UserWhere
+  ) {
+    users(
+      options: { limit: $first, skip: $offset, sort: $orderBy }
+      where: $filter
+    ) {
+      id: userId
+      name
+      avgStars
+      numReviews
+    }
+  }
+`
+
+function UserList(props: any) {
+  const { classes } = props
+  const [order, setOrder] = React.useState<'asc' | 'desc'>('asc')
+  const [orderBy, setOrderBy] = React.useState('name')
+  const [page] = React.useState(0)
+  const [rowsPerPage] = React.useState(10)
+  const [filterState, setFilterState] = React.useState({ usernameFilter: '' })
+
+  const getFilter = () => {
+    return filterState.usernameFilter.length > 0
+      ? { name_CONTAINS: filterState.usernameFilter }
+      : {}
+  }
+
+  const { loading, data, error } = useQuery(GET_USER, {
+    variables: {
+      first: rowsPerPage,
+      offset: rowsPerPage * page,
+      orderBy: { [orderBy]: order.toUpperCase() },
+      filter: getFilter(),
+    },
+  })
+
+  const handleSortRequest = (property: any) => {
+    const newOrderBy = property
+    let newOrder: SortDirection = 'desc'
+
+    if (orderBy === property && order === 'desc') {
+      newOrder = 'asc'
+    }
+
+    setOrder(newOrder)
+    setOrderBy(newOrderBy)
+  }
+
+  const handleFilterChange = (filterName: any) => (event: any) => {
+    const val = event.target.value
+
+    setFilterState((oldFilterState) => ({
+      ...oldFilterState,
+      [filterName]: val,
+    }))
+  }
+
+  return (
+    <Paper className={classes.root}>
+      <Title>User List</Title>
+      <TextField
+        id="search"
+        label="User Name Contains"
+        className={classes.textField}
+        value={filterState.usernameFilter}
+        onChange={handleFilterChange('usernameFilter')}
+        margin="normal"
+        variant="outlined"
+        type="text"
+        InputProps={{
+          className: classes.input,
+        }}
+      />
+      {loading && !error && <p>Loading...</p>}
+      {error && !loading && <p>Error</p>}
+      {data && !loading && !error && (
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                key="name"
+                sortDirection={orderBy === 'name' ? order : false}
+              >
+                <Tooltip title="Sort" placement="bottom-start" enterDelay={300}>
+                  <TableSortLabel
+                    active={orderBy === 'name'}
+                    direction={order}
+                    onClick={() => handleSortRequest('name')}
+                  >
+                    Name
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+              <TableCell
+                key="avgStars"
+                sortDirection={orderBy === 'avgStars' ? order : false}
+              >
+                Average Stars
+              </TableCell>
+              <TableCell
+                key="numReviews"
+                sortDirection={orderBy === 'numReviews' ? order : false}
+              >
+                Number of Reviews
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.users.map((n: any) => {
+              return (
+                <TableRow key={n.id}>
+                  <TableCell component="th" scope="row">
+                    {n.name}
+                  </TableCell>
+                  <TableCell>
+                    {n.avgStars ? n.avgStars.toFixed(2) : '-'}
+                  </TableCell>
+                  <TableCell>{n.numReviews}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      )}
+    </Paper>
+  )
+}
+
+export default withStyles(styles)(UserList)
